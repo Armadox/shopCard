daa876e107f47c482c26b5c5697f84b4e228923e
ady dan <adydan1802@gmail.com>
2023-06-08 14:01:37 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import React from 'react';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <div>
+        <Image src="/images/gm_background.png" alt="Background Image" height={5000} width={5000} style={{objectFit:"cover"}} className="background-image"/>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
+}
